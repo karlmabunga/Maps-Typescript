84a98c346187504f33180323682b8b1a0135d28f
Karl Mabunga <karlmabunga@yahoo.com>
2021-01-29 00:05:06 -0800

    Added a div with id of map to pinpoint a new map and instantiate a new google.maps.Map

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { User } from './User';
-import { Company } from './Company'
+// import { User } from './User';
+// import { Company } from './Company'
 
-const user = new User();
 
-const company = new Company();
-console.log(user, company);
-
-google
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+ });
